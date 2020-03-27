e9abf2913c7cd089706bce08ad5a614f8579ddd7
kungo000 <kungo2009@hotmail.com>
2020-03-27 23:41:52 +0700

    refector
    refector code from i create sub function to handle only 1 thing and get to big function CustomElement

--- CustomElement.js
+++ CustomElement.js
@@ -1,61 +1,60 @@
 function CustomElementMarkDown(tag) {
-  function h1Element(text) {
-    return "<h1>" + text + "</h1>" + "[CUT]";
-  }
-  function h2Element(text) {
-    return "<h2>" + text + "</h2>" + "[CUT]";
-  }
-  function h3Element(text) {
-    return "<h3>" + text + "</h3>" + "[CUT]";
-  }
-  function h4Element(text) {
-    return "<h4>" + text + "</h4>" + "[CUT]";
-  }
-  function h5Element(text) {
-    return "<h5>" + text + "</h5>" + "[CUT]";
-  }
-  function h6Element(text) {
-    return "<h6>" + text + "</h6>" + "[CUT]";
-  }
-  function pElement(text) {
-    return "<p>" + text + "</p>" + "[CUT]";
-  }
-  function liElement(text) {
-    return "<li>" + text + "</li>" + "[CUT]";
-  }
-  function imageElement(src) {
-    return `<img src="${src}"  />[CUT]`;
-  }
   const content = document.querySelector(tag);
+  const { hElement, pElement, liElement, imageElement } = eachOfTypeElement();
   function customTextFunc(text) {
     const splitText = text.split("\n");
     let repText = [];
-    if (splitText.length) {
+    const splitTextLengthMoreThen0 = () => {
       splitText.forEach(text_ => {
         const regex = /######|#####|####|###|##|#|-|img|size|\w/;
         const textNow = text_.replace(regex, function(x, val2, val3) {
           const splitX = val3.replace(regex, "");
           const textT = splitX.trim();
-          const elementFunc = {
-            "#": h1Element(textT),
-            "##": h2Element(textT),
-            "###": h3Element(textT),
-            "####": h4Element(textT),
-            "#####": h5Element(textT),
-            "######": h6Element(textT),
-            "-": liElement(textT),
-            img: imageElement(textT)
-          };
-
-          return elementFunc[x] ? elementFunc[x] : x + pElement(textT);
+          const elementFunc = mapFWithTag(textT);
+          return elementFunc[x] ? elementFunc[x] : pElement(`${x}${textT}`);
         });
-        const split_ = textNow.split("[CUT]");
+        const split_ = textNow.split("[-CUT-]");
         repText += split_[0];
       });
-    }
+    };
+    splitText.length && splitTextLengthMoreThen0();
     content.innerHTML = repText;
+  }
+
+  function mapFWithTag(textT) {
+    return {
+      "#": hElement(textT, 1),
+      "##": hElement(textT, 2),
+      "###": hElement(textT, 3),
+      "####": hElement(textT, 4),
+      "#####": hElement(textT, 5),
+      "######": hElement(textT, 6),
+      "-": liElement(textT),
+      img: imageElement(textT)
+    };
   }
   return {
     text: customTextFunc
+  };
+}
+
+function eachOfTypeElement() {
+  function hElement(text, sizeH) {
+    return `<h${sizeH}>${text}</h${sizeH}>[CUT]`;
+  }
+  function pElement(text) {
+    return "<p>" + text + "</p>" + "[-CUT-]";
+  }
+  function liElement(text) {
+    return "<li>" + text + "</li>" + "[-CUT-]";
+  }
+  function imageElement(src) {
+    return `<img src="${src}"  />[-CUT-]`;
+  }
+  return {
+    hElement,
+    pElement,
+    liElement,
+    imageElement
   };
 }
